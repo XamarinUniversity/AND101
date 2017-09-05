995212dafdc350c2004dc43845637b80cd2c4754
Adam Patridge <patridge@gmail.com>
2017-09-05 11:33:35 -0600

    Initial course exercises import.

---
+++ Exercise 2/Completed/TipCalculator/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TipCalculator
+{
+	[Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
---
+++ Exercise 3/Completed/TipCalculator/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace TipCalculator
+{
+	[Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		EditText inputBill;
+		Button   calculateButton;
+		TextView outputTip;
+		TextView outputTotal;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Main);
+
+			inputBill       = FindViewById<EditText>(Resource.Id.inputBill);
+			calculateButton = FindViewById<Button  >(Resource.Id.calculateButton);
+			outputTip       = FindViewById<TextView>(Resource.Id.outputTip);
+			outputTotal     = FindViewById<TextView>(Resource.Id.outputTotal);
+
+			calculateButton.Click += OnCalculateClick;
+		}
+
+		void OnCalculateClick(object sender, EventArgs e)
+		{
+			string text = inputBill.Text;
+
+			double bill;
+
+			if (double.TryParse(text, out bill))
+			{
+
+				var tip = bill * 0.15;
+				var total = bill + tip;
+
+				outputTip.Text = tip.ToString("C");
+				outputTotal.Text = total.ToString("C");
+			}
+		}
+	}
+}
